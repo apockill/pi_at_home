1195a5917f42a936a2098740ddb9dfae4b2bbee0
Alex Thiele <alex@urbanmachine.build>
2024-12-08 14:14:03 -0800

    Run initial linting

--- launch-profiles/teleop_myarm/launcher.py
+++ launch-profiles/teleop_myarm/launcher.py
@@ -4,13 +4,12 @@ from pathlib import Path
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from pydantic import BaseModel
-
-from node_helpers import launching
-from node_helpers.parameters import ParameterLoader
 
 # Import the forklift URDF module so it register itself with the URDFConstants
-from myarm_ai import example_urdf
+from node_helpers import launching
+from node_helpers.parameters import ParameterLoader
+from pydantic import BaseModel
+
 
 class MetaParameters(BaseModel):
     """This is a great place to put parameters that affect the generation of the launch
@@ -39,9 +38,7 @@ def generate_launch_description() -> LaunchDescription:
     rviz_config = launching.required_file("/robot/launch-profile/rviz-config.rviz")
 
     urdf_node_factories = (
-        launching.URDFModuleNodeFactory(
-            parameters=node_factory_params
-        )
+        launching.URDFModuleNodeFactory(parameters=node_factory_params)
         for node_factory_params in param_loader.meta_parameters.urdf_modules_to_load
     )
     urdf_nodes = []
