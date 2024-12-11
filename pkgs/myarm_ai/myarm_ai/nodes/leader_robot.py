e487f78e1a1173f8225c0686a6f04a9f2cc11a91
Alex Thiele <alex@urbanmachine.build>
2024-12-10 16:34:55 -0800

    Use robot protocol for setting joints

--- pkgs/myarm_ai/myarm_ai/nodes/leader_robot.py
+++ pkgs/myarm_ai/myarm_ai/nodes/leader_robot.py
@@ -6,18 +6,21 @@ from pydantic import BaseModel
 from rclpy.qos import qos_profile_services_default
 from sensor_msgs.msg import JointState
 
+from myarm_ai.robot_protocols import BaseRobotProtocol
 from myarm_ai.urdfs import MyArmCURDF
 
 
 class RobotLeaderNode(HelpfulNode):
     class Parameters(BaseModel):
         joint_publish_hz: float = 10.0
+        robot_protocol: type[BaseRobotProtocol]
 
     def __init__(self, **kwargs: Any):
         super().__init__("RobotLeaderNode", **kwargs)
         # Load parameters from the ROS parameter server
         self.params = self.declare_from_pydantic_model(self.Parameters, "root_config")
         self.urdf = MyArmCURDF
+        self.robot_protocol = self.params.robot_protocol()
 
         # Create publishers
         self.joint_state_publisher = self.create_publisher(
@@ -27,29 +30,18 @@ class RobotLeaderNode(HelpfulNode):
         # Create timers
         self.create_timer(1 / self.params.joint_publish_hz, self.on_publish_joints)
 
-        # For kicks, move joints forwards and backwards on repeat
-        # Mapping of dict[joint_name, tuple(position, velocity)]
-        self.joint_positions = {
-            self.urdf.joints.JOINT_1: (0.0, 0.01),
-            self.urdf.joints.JOINT_2: (0.0, 0.02),
-            self.urdf.joints.JOINT_3: (0.0, 0.03),
-            self.urdf.joints.JOINT_4: (0.0, 0.04),
-            self.urdf.joints.JOINT_5: (0.0, 0.05),
-            self.urdf.joints.JOINT_6: (0.0, 0.06),
-            self.urdf.joints.GRIPPER: (0.0, 0.07),
-        }
-
     def on_publish_joints(self) -> None:
-        joint_positions = {}
-
-        for joint_name, (position, velocity) in self.joint_positions.items():
-            joint_positions[joint_name] = position
-            self.joint_positions[joint_name] = (position + velocity, velocity)
-
-            if position > 1.0:
-                self.joint_positions[joint_name] = (1.0, -velocity)
-            elif position < -1.0:
-                self.joint_positions[joint_name] = (-1.0, velocity)
+        # Map the joint names to the joint positions from the robot protocol
+        joint_readings = self.robot_protocol.read_joints()
+        joint_positions = {
+            self.urdf.joints.JOINT_1: joint_readings[0],
+            self.urdf.joints.JOINT_2: joint_readings[1],
+            self.urdf.joints.JOINT_3: joint_readings[2],
+            self.urdf.joints.JOINT_4: joint_readings[3],
+            self.urdf.joints.JOINT_5: joint_readings[4],
+            self.urdf.joints.JOINT_6: joint_readings[5],
+            self.urdf.joints.GRIPPER: joint_readings[6],
+        }
 
         joint_state = JointState()
         joint_state.name = list(joint_positions.keys())
