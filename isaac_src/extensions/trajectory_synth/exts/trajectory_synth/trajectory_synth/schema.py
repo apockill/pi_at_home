8dcd8ac999d9b7f46245d7d16c23a0357a9c1652
Alex Thiele <alex@urbanmachine.build>
2024-12-25 10:03:17 -0800

    Update textures directory

--- isaac_src/extensions/trajectory_synth/exts/trajectory_synth/trajectory_synth/schema.py
+++ isaac_src/extensions/trajectory_synth/exts/trajectory_synth/trajectory_synth/schema.py
@@ -6,9 +6,7 @@ from pydantic import BaseModel
 TIMESTEPS_FILENAME = "timesteps"  # usd
 SCENE_FILENAME = "scene"  # usd
 METADATA_FILENAME = "metadata.json"
-DEFAULT_MESH_TEXTURES_DIR = Path(
-    "/robot/isaac_src/assets/textures/512x512-photorealistic"
-)
+DEFAULT_MESH_TEXTURES_DIR = Path("/robot/isaac_src/assets/textures/random")
 DEFAULT_SKYBOX_TEXTURES_DIR = Path("/robot/isaac_src/assets/textures/skyboxes")
 DEFAULT_RECORDINGS_DIR = Path("/robot/synthetic-output/recordings/")
 
