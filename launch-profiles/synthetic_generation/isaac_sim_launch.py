9f3b4d40774104905d7c0651044d1e6012bcb517
Alex Thiele <alex@urbanmachine.build>
2024-12-29 15:17:18 -0800

    Fix lint

--- launch-profiles/synthetic_generation/isaac_sim_launch.py
+++ launch-profiles/synthetic_generation/isaac_sim_launch.py
@@ -1,9 +1,7 @@
 import omni.kit.app
 import omni.usd
 
-omni.usd.get_context().open_stage(
-    "/robot/isaac_src/assets/scenes/cube-to-cup-task.usd"
-)
+omni.usd.get_context().open_stage("/robot/isaac_src/assets/scenes/cube-to-cup-task.usd")
 
 # Start the simulation timeline on startup
 timeline = omni.timeline.get_timeline_interface()
--- launch-profiles/teleop_myarm/isaac_sim_launch.py
+++ launch-profiles/teleop_myarm/isaac_sim_launch.py
@@ -1,9 +1,7 @@
 import omni.kit.app
 import omni.usd
 
-omni.usd.get_context().open_stage(
-    "/robot/isaac_src/assets/scenes/simple-teleop.usd"
-)
+omni.usd.get_context().open_stage("/robot/isaac_src/assets/scenes/simple-teleop.usd")
 
 # Start the simulation timeline on startup
 timeline = omni.timeline.get_timeline_interface()
